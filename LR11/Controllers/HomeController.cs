e11ff096996a149474370f76fa882fd314c28029
Artem Ovcharenko <artemovcharenko30@gmail.com>
2024-04-15 16:12:39 +0300

    Добавьте файлы проекта.

---
+++ LR11/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LR11.Controllers
+{
+	[UniqueUsersFilter]
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ LR11/LoginFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.IO;
+
+namespace LR11
+{
+	public class LogActionFilter : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			string controllerName = context.ActionDescriptor.DisplayName;
+			string actionName = context.ActionDescriptor.DisplayName;
+
+			
+			string logMessage = $"Date: {DateTime.Now} |  Action: {actionName} |  Controller: {controllerName}";
+
+			string filePath = "Log.txt";
+			File.AppendAllText(filePath, logMessage + Environment.NewLine);
+
+			base.OnActionExecuting(context);
+		}
+	}
+}
